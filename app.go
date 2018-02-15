bf5c437138a13afce9fcb85c246e8445e23efe6f
kik4 <kikkikkikkik@users.noreply.github.com>
2018-02-15 23:40:01 +0900

    feat: index template

--- app.go
+++ app.go
@@ -1,19 +1,22 @@
-package main
+package app
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/kikkikkikkik/ocelot"
 )
 
 func init() {
+	t := template.Must(template.ParseFiles("templates/index.tmpl"))
 
 	var o = ocelot.New()
 
 	o.Register("get", "/", func(w http.ResponseWriter, r *http.Request) error {
-		fmt.Fprintf(w, "Welcome!")
-		return nil
+		return t.Execute(w, map[string]interface{}{
+			"Language": "Golang",
+			"Stars":    5,
+		})
 	})
 
 	http.Handle("/", o)
